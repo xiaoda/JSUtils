244c84b0c057c2dc1ec4064d4650afc910feb63d
Lynch Tai <lqdai.xiaoda@gmail.com>
2022-08-04 17:00:48 +0800

    Create api doc with apidoc

--- routes/date.js
+++ routes/date.js
@@ -2,6 +2,20 @@ const express = require('express')
 const router = express.Router()
 const getWorkingHours = require('../core/date/working-hours')
 
+/**
+ * @api {get} /date/working-hours 计算工作时间
+ * @apiGroup Date
+ * 
+ * @apiQuery {String} startDateTime 开始日期时间
+ * @apiQuery {String} endDateTime 结束日期时间
+ * @apiQuery {Array} [countingPeriods] 自定义上下班时间
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "hours": 8
+ *     }
+ */
 router.get('/working-hours', function(req, res, next) {
   let {startDateTime, endDateTime, countingPeriods} = req.query
   if (countingPeriods) countingPeriods = eval(countingPeriods)
